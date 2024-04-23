f4a23a5d4b8b19bf4dfd1ad50e5b41a8320efc70
Dean Crowley <deancrowley1@gmail.com>
2024-04-23 18:23:45 +0100

    Updated Components & UI

--- src/components/Obituary.js
+++ src/components/Obituary.js
@@ -7,16 +7,19 @@ const Obituary = () => {
     <View style={styles.container}>
       <Text as="h1" style={styles.heading}>In Loving Memory</Text>
       <Text as="p" style={styles.paragraph}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        John Smith, aged 74, of Tramore, Waterford, Ireland, peacefully departed this world on January 1, 2024, surrounded by the warmth of his loved ones.
       </Text>
       <Text as="p" style={styles.paragraph}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Born on a brisk New Year’s Day in 1950 in Tramore, Waterford, John was the beloved son of Patrick and Siobhan Smith. Raised along the picturesque Irish coastline, John's childhood was marked by the wild beauty of the sea and the close-knit community of his hometown.
       </Text>
       <Text as="p" style={styles.paragraph}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        John and Aisling were blessed with three children: Eoin, born in 1981, Niamh, born in 1984, and Ciara, born in 1987. As a father, John was deeply engaged, nurturing, and ever-present. He passed on his love for Irish culture and history to his children, ensuring they knew the rich tapestry of their heritage.
+      </Text>
+      <Text as="p" style={styles.paragraph}>
+        Special thanks are extended to John's caregivers during his final days, whose kindness and dedication were a great comfort. We also express our gratitude to the extended family and friends who supported us with love and care during this difficult time.
       </Text>
     </View>
-  );
+  );  
 };
 
 const styles = {
@@ -28,12 +31,12 @@ const styles = {
     },
     heading: {
       paddingBottom: '2rem',
-      fontSize: '2rem',
+      fontSize: '2.5rem',
       textAlign: 'center',
     },
     paragraph: {
       padding: '0.375rem 0.75rem',
-      fontSize: '1.25rem',
+      fontSize: '1.85rem',
       lineHeight: '1.5',
       margin: '1rem 0',
       width: 'calc(100% - 1.5rem)', 
--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
 import { View, Image, Text } from '@aws-amplify/ui-react';
-import Picture from '../images/JohnSmith.jpg';
+import image1 from '../images/image1.jpg';
+import image2 from '../images/image2.jpg';
+import image3 from '../images/image3.jpg';
+import image4 from '../images/image4.jpg';
+import image5 from '../images/image5.jpg';
+import image6 from '../images/image6.jpg';
 
 const photos = [
-  Picture,
-  Picture,
-  Picture,
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6
 ];
 
 const PhotoGallery = () => {
